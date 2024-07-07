51d154fa12ebab36bd6a19010816028a7b31625d
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2024-07-07 09:01:55 +0530

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -1,5 +1,7 @@
 public class Numbers{
+    // Printing the number from 1-10 in the program
     public static void main(String[] args) {
+        system.out.print("Prints the number from 1-10")
         for( int i = 0; i < 11; i++){
             System.out.println(i +"");
         }
